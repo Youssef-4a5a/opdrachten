547a2c0b48ace98aa531bf6cc891824af99f19c7
Youssef-4a5a <430149@edu.rocmn.nl>
2017-02-16 16:14:48 +0100

    aanpassingen aan functies

--- beoordelingenlijst.js
+++ beoordelingenlijst.js
@@ -4,18 +4,17 @@
 function resultaat(cijferke){
     var cijfer = document.getElementById("field").value;
     
+     if((cijfer > 100) && (cijfer < 0)){
+         cijferke = 0;
+        document.getElementById("resultaat").innerHTML = "Het cijfer is niet geldig. Voer een geldige cijfer in";
+    }
+    
     if(cijfer <= 49){
         var result = "onvoldoende";
-        
     } else if((cijfer >= 50) && (cijfer <= 59)){
         var result = "matig";
-        document.getElementById("resultaat").innerHTML = "Het cijfer is "+cijfer+" en is dus "+result;
     } else if((cijfer >= 60) && (cijfer <= 74)){
         var result = "voldoende";
-        document.getElementById("resultaat").innerHTML = "Het cijfer is "+cijfer+" en is dus "+result;
-    } else{
-        var result = "goed";
-        document.getElementById("resultaat").innerHTML = "Het cijfer is "+cijfer+" en is dus "+result;
     }
     
     if(cijferke === 1){
